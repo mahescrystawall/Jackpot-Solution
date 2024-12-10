71c48f209883d52b1ea1d67d29ba74802c18150f
Tuan Khaazen Burah <tuan.k@crystawall.com>
2024-12-10 12:53:30 +0400

    FLOWBITE integration

--- Jackpot.Web/tailwind.config.js
+++ Jackpot.Web/tailwind.config.js
@@ -3,11 +3,10 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+      "./node_modules/flowbite/**/*.js"
     ],
     theme: {
         extend: {
@@ -16,5 +15,8 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
+
 };
