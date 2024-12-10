1d31a7acd033806d66f5d942d01e8d9cb66bdc13
mahescrystawall <maheshwari.marimuthu@crystawall.com>
2024-12-10 13:49:31 +0400

    changed the api url

--- Jackpot.Api/app/Services/LoginService.php
+++ Jackpot.Api/app/Services/LoginService.php
@@ -1,29 +1,18 @@
 <?php
-
 namespace App\Services;
+
 use App\Interfaces\ILoginService;
-use Illuminate\Support\Facades\File;
 use App\Traits\FileHelper;
 
 class LoginService implements ILoginService
 {
-
     use FileHelper;
 
-    /**
-     * Fetch the data from the given JSON file.
-     *
-     * @param string $fileName
-     * @return array
-     */
-    /**
-     * Fetch the data from the given JSON file.
-     *
-     * @param string $fileName
-     * @return array
-     */
-    public function getLoginData(string $fileName)
+    public function getLoginData(string $fileName): array
     {
         return $this->getDataFromFile($fileName);
     }
 }
+
+
+
