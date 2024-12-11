93c8e35ecb38aac43c2b5b0cb6a0a213c3c1775e
Amrutha <amruthaur99@gmail.com>
2024-12-11 18:14:25 +0400

    header

--- Jackpot.Web/resources/views/layouts/header.blade.php
+++ Jackpot.Web/resources/views/layouts/header.blade.php
@@ -1,21 +1,54 @@
-
-
-<nav class="fixed top-0 z-50 w-full bg-jcolor4 border-b text-jwhite1">
-  <div class="px-3 py-3 lg:px-5 lg:pl-3">
-    <div class="flex items-center justify-between">
-      <div class="flex items-center justify-start rtl:justify-end">
-        <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+<nav class="bg-jcolor4 fixed w-full border-b border-r border-jblue2 top-0 z-50 text-jwhite1">
+  <div class="px-3 lg:px-5 lg:pl-3">
+    <div class="flex items-center justify-between ">
+      <div class="w-auto md:w-[244px] md:border-r md:border-jblue2 py-3 flex items-center">
+        
+          <button aria-controls="logo-sidebar"
+            type="button"
+            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
             <span class="sr-only">Open sidebar</span>
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-               <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path clip-rule="evenodd" fill-rule="evenodd"
+                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75a0 010 1.5H2.75A.75.75 0 012 10z">
+              </path>
             </svg>
-         </button>
+        </button>
         <a href="" class="flex ms-2 md:me-24">
           <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap">JACKPOT</span>
         </a>
       </div>
-      <div class="flex items-center">
-          <div class="flex items-center ms-3">
+      <div class="flex items-center py-3">
+        <div class="flex items-center ms-3">
+          <div class="relative group">
+            <!-- Trigger -->
+            <button type="button" class="flex items-center text-sm">
+              <span class="sr-only">Open user menu</span>
+              <img class="w-9 h-9 rounded-sm" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="user photo">
+              <div class="flex items-center space-x-1 ms-2">
+                <span class="hidden md:block text-jwhite1 rounded-lg text-sm px-2">Bonnie Green</span>
+                <svg height="12" width="12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                </svg>
+              </div>
+            </button>
+
+            <!-- Dropdown menu -->
+            <div class="absolute right-2 hidden z-50 my-4 text-base list-none bg-jcolor4 divide-y divide-gray-100 shadow group-hover:block">
+              <ul class="px-4 py-4 space-y-2" aria-labelledby="user-menu-button">
+                <li><a href="#" class="block text-sm text-jwhite1">Account Statement</a></li>
+                <li><a href="#" class="block text-sm text-jwhite1">Profit & Loss</a></li>
+                <li><a href="#" class="block text-sm text-jwhite1">Bet History</a></li>
+                <li><a href="#" class="block text-sm text-jwhite1">Un-Settled Bets</a></li>
+                <li><a href="#" class="block text-sm text-jwhite1">Set Button Values</a></li>
+                <li><a href="#" class="block text-sm text-jwhite1">Change Password</a></li>
+                <li><a href="#" class="block text-sm text-jwhite1">Log out</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+          <!-- <div class="flex items-center ms-3">
             <div>
               <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                 <span class="sr-only">Open user menu</span>
@@ -46,8 +79,99 @@
                 </li>
               </ul>
             </div>
-          </div>
+          </div> -->
         </div>
     </div>
-  </div>
+
+
 </nav>
+
+<!-- <div class="flex flex-col">
+  <div class="flex flex-col justify-center w-full max-md:max-w-full">
+    <div
+      class="flex flex-col w-full bg-black shadow-[0px_2px_4px_rgba(3,119,124,1)] max-md:max-w-full"
+      role="banner"
+    >
+      <div
+        class="flex flex-wrap gap-10 justify-between w-full max-w-[1426px] min-h-[82px] max-md:max-w-full"
+      >
+        <div
+          class="flex items-center self-end w-60 text-2xl font-thin leading-none whitespace-nowrap border-r border-gray-800 text-neutral-400"
+        >
+          <div
+            class="flex flex-col justify-center items-center self-stretch px-2 py-3.5 my-auto w-60 min-h-[82px]"
+          >
+            <div class="flex gap-1 items-center">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/b19d10cba04e44e6a290692865305dd3/0d583c87d871831c87e17c3bc986db7f28d08e1d437b411452223c21ce9eb0ad?apiKey=b19d10cba04e44e6a290692865305dd3&"
+                alt="99hubspot company logo"
+                class="object-contain shrink-0 self-stretch my-auto rounded-md aspect-[1.19] w-[31px]"
+              />
+              <div class="gap-2.5 self-stretch px-2 py-5 my-auto min-h-[54px]">
+                99hubspot
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex gap-8 justify-center items-center h-full text-xs min-w-[240px] text-zinc-100"
+        >
+          <button
+            class="flex flex-col justify-center self-stretch px-2.5 py-1.5 my-auto whitespace-nowrap bg-teal-700 rounded-md shadow-[3px_3px_0px_rgba(0,173,181,1)]"
+            tabindex="0"
+          >
+            <div class="gap-2.5 self-stretch py-0.5 min-h-[18px]">Deposit</div>
+          </button>
+          <div
+            class="flex flex-col justify-center self-stretch my-auto w-[61px]"
+            role="group"
+            aria-label="Exposure Information"
+          >
+            <div
+              class="flex-1 shrink gap-2.5 self-stretch w-full whitespace-nowrap"
+            >
+              Exposure
+            </div>
+            <div class="gap-2.5 self-stretch py-0.5 min-h-[18px]">
+              $ 130, 000
+            </div>
+          </div>
+          <div
+            class="flex flex-col justify-center self-stretch text-white w-[70px]"
+            role="group"
+            aria-label="Balance Information"
+          >
+            <div class="gap-2.5 self-stretch w-full whitespace-nowrap">
+              Balance
+            </div>
+            <div class="gap-2.5 self-stretch w-full">$ 6,430,000</div>
+          </div>
+          <div
+            class="flex flex-col self-stretch my-auto text-white min-h-[46px]"
+            role="group"
+            aria-label="User Profile"
+          >
+            <div class="flex flex-1 gap-1.5 items-center h-full">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/b19d10cba04e44e6a290692865305dd3/38697418a01106f76a7817ffe048c97de05aed681a98faef6856fcd29709a096?apiKey=b19d10cba04e44e6a290692865305dd3&"
+                alt="Profile picture of Aron B."
+                class="object-contain shrink-0 self-stretch my-auto rounded-md aspect-square w-[47px]"
+              />
+              <div class="flex gap-2 items-start self-stretch my-auto">
+                <div class="self-stretch">Aron B.</div>
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/b19d10cba04e44e6a290692865305dd3/c9bd9618ff84f96a493a20cdb955d8b739c451023412748805701e3f06c6e7d9?apiKey=b19d10cba04e44e6a290692865305dd3&"
+                  alt=""
+                  class="object-contain shrink-0 w-4 aspect-square"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
--- Jackpot.Web/resources/views/layouts/header/header.blade.php
+++ Jackpot.Web/resources/views/layouts/header/header.blade.php
@@ -1,179 +1,73 @@
-<nav class="bg-jcolor4 fixed w-full border-b border-r border-jblue2 top-0 z-50 text-jwhite1">
-  <div class="px-3 py-3 lg:px-5 lg:pl-3">
+
+
+<nav class="fixed top-0 z-50 w-full bg-jcolor4 border-b text-jwhite1">
+  <div class="px-3 lg:px-5 lg:pl-3">
     <div class="flex items-center justify-between">
-      <div class="flex items-center justify-start rtl:justify-end">
-        <button type="button"
-          class="text-white bg-jblue2 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">JP</button>
-        <button aria-controls="logo-sidebar"
-          type="button"
-          class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
-          <span class="sr-only">Open sidebar</span>
-          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path clip-rule="evenodd" fill-rule="evenodd"
-              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75a0 010 1.5H2.75A.75.75 0 012 10z">
-            </path>
-          </svg>
-        </button>
+      <div class="w-auto md:w-[244px] md:border-r md:border-jblue2 py-5 flex items-center">
+        <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
+            <span class="sr-only">Open sidebar</span>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+               <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+            </svg>
+         </button>
         <a href="" class="flex ms-2 md:me-24">
           <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap">JACKPOT</span>
         </a>
       </div>
-      <div class="flex items-center">
-        <div class="flex items-center ms-3">
-          <div class="relative group">
-            <!-- Trigger -->
-            <button type="button" class="flex text-sm  focus:ring-4 focus:ring-gray-300">
-              <span class="sr-only">Open user menu</span>
-              <img class="w-9 h-9" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="user photo">
-              <div class="flex items-center space-x-2 ms-2">
-                <span class=" text-jwhite1 rounded-lg text-sm px-5 py-2.5  mb-2">Bonnie Green</span><svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-  </svg>
-              </div>
-            </button>
 
-            <!-- Dropdown menu -->
-            <div
-              class="absolute right-0 hidden z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow group-hover:block dark:bg-gray-700 dark:divide-gray-600">
-              <div class="px-4 py-3">
-                <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-                <span class="block text-sm text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-              </div>
-              <ul class="py-2" aria-labelledby="user-menu-button">
-                <li>
-                  <a href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-          <!-- <div class="flex items-center ms-3">
-            <div>
-              <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                <span class="sr-only">Open user menu</span>
-                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
+
+      <div class="flex items-center py-3">
+
+        <div class="grid md:grid-cols-[100px_100px_100px_150px] md:gap-3 " >
+          <div class="hidden md:flex w-[100px] justify-center items-center h-full text-xs">
+            <button
+                class="flex flex-col justify-center self-stretch px-2.5 py-1.5 my-auto whitespace-nowrap bg-jblue2 rounded-md shadow-[3px_3px_0px_rgba(0,173,181,1)]"
+                tabindex="0">
+                <div class=" self-stretch py-0.5 min-h-[18px]">Deposit</div>
               </button>
-            </div>
-            <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
-              <div class="px-4 py-3" role="none">
-                <p class="text-sm text-gray-900 dark:text-white" role="none">
-                  Neil Sims
-                </p>
-                <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                  neil.sims@flowbite.com
-                </p>
+          </div>
+
+          <div class="flex md:flex-col justify-center md:w-[100px] gap-2 md:gap-0">
+            <div class="flex-1">Exposure</div>
+            <div class=" self-stretch py-0.5 min-h-[18px]">$ 130, 000</div>
+          </div>
+
+          <div class="flex md:flex-col justify-center md:w-[100px] gap-2 md:gap-0">
+            <div class="flex-1">Balance</div>
+            <div class=" self-stretch py-0.5 min-h-[18px]">$ 6,430,000</div>
+          </div>
+
+          <div class="hidden md:flex flex-col justify-center">
+            <div class="flex items-center ms-3">
+              <div>
+                <button type="button" class="flex text-sm items-center" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                  <span class="sr-only">Open user menu</span>
+                  <img class="w-9 h-9 rounded-md" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="user photo">
+                  <div class="flex items-center space-x-1 ms-2">
+                      <span class="text-jwhite1  text-sm px-2">Bonnie Green</span>
+                      <svg height="12" width="12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                      </svg>
+                  </div>
+                </button>
               </div>
-              <ul class="py-1" role="none">
-                <li>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
-                </li>
-                <li>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
-                </li>
-                <li>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
-                </li>
-                <li>
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
-                </li>
-              </ul>
-            </div>
-          </div> -->
-        </div>
-    </div>
-
-
-</nav>
-
-<!-- <div class="flex flex-col">
-  <div class="flex flex-col justify-center w-full max-md:max-w-full">
-    <div
-      class="flex flex-col w-full bg-black shadow-[0px_2px_4px_rgba(3,119,124,1)] max-md:max-w-full"
-      role="banner"
-    >
-      <div
-        class="flex flex-wrap gap-10 justify-between w-full max-w-[1426px] min-h-[82px] max-md:max-w-full"
-      >
-        <div
-          class="flex items-center self-end w-60 text-2xl font-thin leading-none whitespace-nowrap border-r border-gray-800 text-neutral-400"
-        >
-          <div
-            class="flex flex-col justify-center items-center self-stretch px-2 py-3.5 my-auto w-60 min-h-[82px]"
-          >
-            <div class="flex gap-1 items-center">
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/b19d10cba04e44e6a290692865305dd3/0d583c87d871831c87e17c3bc986db7f28d08e1d437b411452223c21ce9eb0ad?apiKey=b19d10cba04e44e6a290692865305dd3&"
-                alt="99hubspot company logo"
-                class="object-contain shrink-0 self-stretch my-auto rounded-md aspect-[1.19] w-[31px]"
-              />
-              <div class="gap-2.5 self-stretch px-2 py-5 my-auto min-h-[54px]">
-                99hubspot
+              <div class="z-50 hidden my-4 text-base list-none bg-jcolor4 divide-y divide-gray-100 rounded shadow" id="dropdown-user">
+            
+                <ul class="py-3 px-4" role="none">
+                  <li><a href="#" class="block px-4 py-2 text-sm text-jwhite1" role="menuitem">Account Statement</a></li>
+                  <li><a href="#" class="block px-4 py-2 text-sm text-jwhite1" role="menuitem">Profit & Loss</a></li>
+                  <li><a href="#" class="block px-4 py-2 text-sm text-jwhite1" role="menuitem">Bet History</a></li>
+                  <li><a href="#" class="block px-4 py-2 text-sm text-jwhite1" role="menuitem">Unsettled Bets</a></li>
+                  <li><a href="#" class="block px-4 py-2 text-sm text-jwhite1" role="menuitem">Set Button Values</a></li>
+                  <li><a href="#" class="block px-4 py-2 text-sm text-jwhite1" role="menuitem">Change Password</a></li>
+                  <li><a href="#" class="block px-4 py-2 text-sm text-jwhite1" role="menuitem">Sign out</a></li>
+                </ul>
               </div>
             </div>
           </div>
-        </div>
-        <div
-          class="flex gap-8 justify-center items-center h-full text-xs min-w-[240px] text-zinc-100"
-        >
-          <button
-            class="flex flex-col justify-center self-stretch px-2.5 py-1.5 my-auto whitespace-nowrap bg-teal-700 rounded-md shadow-[3px_3px_0px_rgba(0,173,181,1)]"
-            tabindex="0"
-          >
-            <div class="gap-2.5 self-stretch py-0.5 min-h-[18px]">Deposit</div>
-          </button>
-          <div
-            class="flex flex-col justify-center self-stretch my-auto w-[61px]"
-            role="group"
-            aria-label="Exposure Information"
-          >
-            <div
-              class="flex-1 shrink gap-2.5 self-stretch w-full whitespace-nowrap"
-            >
-              Exposure
-            </div>
-            <div class="gap-2.5 self-stretch py-0.5 min-h-[18px]">
-              $ 130, 000
-            </div>
-          </div>
-          <div
-            class="flex flex-col justify-center self-stretch text-white w-[70px]"
-            role="group"
-            aria-label="Balance Information"
-          >
-            <div class="gap-2.5 self-stretch w-full whitespace-nowrap">
-              Balance
-            </div>
-            <div class="gap-2.5 self-stretch w-full">$ 6,430,000</div>
-          </div>
-          <div
-            class="flex flex-col self-stretch my-auto text-white min-h-[46px]"
-            role="group"
-            aria-label="User Profile"
-          >
-            <div class="flex flex-1 gap-1.5 items-center h-full">
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/b19d10cba04e44e6a290692865305dd3/38697418a01106f76a7817ffe048c97de05aed681a98faef6856fcd29709a096?apiKey=b19d10cba04e44e6a290692865305dd3&"
-                alt="Profile picture of Aron B."
-                class="object-contain shrink-0 self-stretch my-auto rounded-md aspect-square w-[47px]"
-              />
-              <div class="flex gap-2 items-start self-stretch my-auto">
-                <div class="self-stretch">Aron B.</div>
-                <img
-                  loading="lazy"
-                  src="https://cdn.builder.io/api/v1/image/assets/b19d10cba04e44e6a290692865305dd3/c9bd9618ff84f96a493a20cdb955d8b739c451023412748805701e3f06c6e7d9?apiKey=b19d10cba04e44e6a290692865305dd3&"
-                  alt=""
-                  class="object-contain shrink-0 w-4 aspect-square"
-                />
-              </div>
-            </div>
-          </div>
+          
         </div>
       </div>
     </div>
   </div>
-</div> -->
+</nav>
