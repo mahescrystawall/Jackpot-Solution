7a69e8f3cf64b826c70dca98a212f0f2b3e79e76
mahescrystawall <maheshwari.marimuthu@crystawall.com>
2024-12-11 17:50:08 +0400

    revert the changes

--- Jackpot.Api/app/Traits/FileHelper.php
+++ Jackpot.Api/app/Traits/FileHelper.php
@@ -2,27 +2,21 @@
 
 namespace App\Traits;
 
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 Trait FileHelper {
-    public function getDataFromFile(string $fileName): array
-    {
-        $path = storage_path('json/' . $fileName);
-
-        if (!Storage::exists($path)) {
-            return ['error_message' => 'File not found'];
+    public function getDataFromFile($fileName)
+        {
+            $path = storage_path('json/' . $fileName);
+            if (file_exists($path)) {
+                $json = file_get_contents($path);
+                $data = json_decode($json, true);
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    return ['message' => 'Invalid JSON data'];
+                }
+                return $data;
+            } else {
+                return ['error_message' => 'File not found'];
+            }
         }
-
-        // Read file contents using Storage facade
-        $json = Storage::get($path);
-
-        // Decode JSON and handle potential errors
-        $data = json_decode($json, true);
-
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            return ['error_message' => 'Invalid JSON data'];
-        }
-
-        return $data;
-    }
 }
