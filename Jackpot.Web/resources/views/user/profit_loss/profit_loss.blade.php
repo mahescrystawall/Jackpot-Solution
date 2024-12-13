6bd7bb24ee83243e21a3c9f32a341611a03d9ff3
Amrutha <amruthaur99@gmail.com>
2024-12-13 16:43:34 +0400

    profit-loss

--- Jackpot.Web/resources/views/user/profit_loss/profit_loss.blade.php
+++ Jackpot.Web/resources/views/user/profit_loss/profit_loss.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 @section('content')
-    @include('layouts.marquee')
+ 
 
-    <div class="mt-200">
-        <div class="p-2 pt-0 sm:ml-64 mt-16">
+    <div class="mt-200 text-white">
+        <div class="p-2 pt-6 sm:ml-64 mt-14 h-screen">
+
+            @include('layouts.marquee')
             <section class="w-full ">
                 <div class="flex flex-row w-full py-2 mb-2 bg-gradient-to-r from-[#00ADB5] via-[#00ADB5] to-[#1B1B1B]">
                     <div class="flex items-center gap-2 text-white text-sm w-full px-2">
