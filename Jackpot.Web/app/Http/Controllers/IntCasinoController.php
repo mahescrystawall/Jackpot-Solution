a100c460262c7a2009ff1f7e726fd795901dbd9c
mahescrystawall <maheshwari.marimuthu@crystawall.com>
2024-12-16 15:01:24 +0400

    Created the int casino controller and service

--- Jackpot.Web/app/Http/Controllers/IntCasinoController.php
+++ Jackpot.Web/app/Http/Controllers/IntCasinoController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\IntCasinoService;
 
 class IntCasinoController extends Controller
 {
+    protected $intCasinoService;
+
+    public function __construct(IntCasinoService $intCasinoService)
+    {
+        $this->intCasinoService = $intCasinoService;
+    }
 
     public function index()
     {
-        return view('int-casino.int-casino');
+        // Fetch data using the service
+        $casinoData = $this->intCasinoService->getCasinoData();
+
+        // Pass the data to the view
+        return view('int-casino.int-casino', compact('casinoData'));
     }
 }
