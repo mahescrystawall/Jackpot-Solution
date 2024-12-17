33a4259e0b562096277b93f109368ddcfec143cd
mahescrystawall <maheshwari.marimuthu@crystawall.com>
2024-12-17 10:38:12 +0400

    bet histroy page update

--- Jackpot.Web/app/Http/Controllers/User/BetHistoryController.php
+++ Jackpot.Web/app/Http/Controllers/User/BetHistoryController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use Carbon\Carbon;
 use App\Services\BetHistoryService;
-
+use Carbon\Carbon;
 class BetHistoryController extends Controller
 {
     protected $betHistoryService;
@@ -19,30 +17,20 @@ class BetHistoryController extends Controller
 
     public function index(Request $request)
     {
-      
         $startDatecal = Carbon::now()->subDays(5)->toDateString();
         $endDatecal = Carbon::now()->toDateString();
         $eventTypeId = $startDate = $endDate = $isMatched = '';
         $events = null;
-
         if ($request->get('event_type_id')) {
             $eventTypeId = $request->get('event_type_id');
             $startDate = $request->get('start_date');
             $endDate = $request->get('end_date');
             $isMatched = $request->get('is_matched', 1);
         }
-
-        $betHistoryData = $this->betHistoryService->getbetHistoryData();
-        if (empty($betHistoryData['error'])) {
-            return view('user/bet_history', [
-                'events' => [],
-                'pagination' => [],
-                'error' => $betHistoryData['error'], // Pass error message to view
-            ]);
-        }
-        $events = $betHistoryData['data']['orders'];
-        $pagination = $betHistoryData['data']['orders']['links'];
+        $allSports = $this->betHistoryService->getAllSports();
+        $betHistoryData = $this->betHistoryService->getbetHistoryData($eventTypeId, $startDate, $endDate, $isMatched);
+        $events = $betHistoryData;
+        $pagination = $betHistoryData;
         return view('user/bet_history/bet_history', compact('events','pagination','startDatecal','endDatecal'));
     }
 }
-?>
