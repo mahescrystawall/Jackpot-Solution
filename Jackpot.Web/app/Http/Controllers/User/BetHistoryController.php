1a980e225a9606b5d523a64a601f1611ce0b3b61
mahescrystawall <maheshwari.marimuthu@crystawall.com>
2024-12-17 11:30:04 +0400

    Bet history static data passed when laod

--- Jackpot.Web/app/Http/Controllers/User/BetHistoryController.php
+++ Jackpot.Web/app/Http/Controllers/User/BetHistoryController.php
@@ -19,17 +19,14 @@ class BetHistoryController extends Controller
     {
         $startDatecal = Carbon::now()->subDays(5)->toDateString();
         $endDatecal = Carbon::now()->toDateString();
-        $eventTypeId = $startDate = $endDate = $isMatched = '';
-        $events = null;
-        if ($request->get('event_type_id')) {
-            $eventTypeId = $request->get('event_type_id');
-            $startDate = $request->get('start_date');
-            $endDate = $request->get('end_date');
-            $isMatched = $request->get('is_matched', 1);
-        }
+        $eventTypeId = 4;
+        $startDate = '2024-01-01';
+        $endDate = Carbon::now()->toDateString();
+        $isMatched = 1;
         $allSports = $this->betHistoryService->getAllSports();
         $betHistoryData = $this->betHistoryService->getbetHistoryData($eventTypeId, $startDate, $endDate, $isMatched);
         $events = $betHistoryData;
+
         $pagination = $betHistoryData;
         return view('user/bet_history/bet_history', compact('events','pagination','startDatecal','endDatecal'));
     }
