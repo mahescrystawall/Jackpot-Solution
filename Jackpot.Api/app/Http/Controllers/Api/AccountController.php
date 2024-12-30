750d71da1fa41c0ca60856bc90ff8b1f2b41357f
Win Khant Aung <win.aung@crystawall.com>
2024-12-30 13:18:23 +0400

    Update logging to use environment variable for log channel in AccountController and ProfitLossApiController

--- Jackpot.Api/app/Http/Controllers/Api/AccountController.php
+++ Jackpot.Api/app/Http/Controllers/Api/AccountController.php
@@ -47,7 +47,7 @@ class AccountController extends Controller
                 200
             );
         } catch (\Throwable $th) {
-            Log::channel('error_logs')->error('An error occurred in the custom log.'.$th);
+            Log::channel(env('LOG_CHANNEL'))->error('An error occurred in the custom log.'.$th);
             return $this->sendError($th);
         }
     }
--- Jackpot.Api/app/Http/Controllers/Api/ProfitLossApiController.php
+++ Jackpot.Api/app/Http/Controllers/Api/ProfitLossApiController.php
@@ -35,7 +35,7 @@ class ProfitLossApiController extends Controller
                 200
             );
         } catch (\Throwable $th) {
-            Log::channel('error_logs')->error('An error occurred in the custom log.'.$th);
+            Log::channel(env('LOG_CHANNEL'))->error('An error occurred in the custom log.'.$th);
             return $this->sendError($th);
         }
     }
